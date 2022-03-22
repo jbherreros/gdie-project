5011f08db9cf385bc882328b8bdea3b43c01a31f
Guillermo Torrens <guillermotorrens5@gmail.com>
2022-03-22 12:02:49 +0100

    Añadido scoreboard y window.onload track.js

--- js/tracks.js
+++ js/tracks.js
@@ -1,10 +1,21 @@
-
-
+/*
 window.addEventListener('DOMContentLoaded', (event) => {
     var textTracks = myVideo.textTracks;
     var textTrack = textTracks[0]; // plays track
     var cues = textTrack.cues; // cues list
     var cue = cues[0];
+    console.log(cues[0]);
     console.log(cue.text);
     console.log(textTrack.kind);
 });
+*/
+//Deja de funcionar en Chrome cuando se recarga la página después de haber cargado inmediatamente la página
+window.onload = function() {
+    var textTracks = myVideo.textTracks;
+    var textTrack = textTracks[0]; // plays track
+    var cues = textTrack.cues; // cues list
+    var cue = cues[0];
+    console.log(cues[0]);
+    console.log(cue.text);
+    console.log(textTrack.kind);
+};
