9b2be3569ae0c710a0286eca546a6cee2dc21667
Guillermo Torrens <guillermotorrens5@gmail.com>
2022-03-24 17:36:18 +0100

    Cambio en plays.vtt

--- js/tracks.js
+++ js/tracks.js
@@ -15,7 +15,7 @@ window.onload = function() {
     var textTrack = textTracks[0]; // plays track
     var cues = textTrack.cues; // cues list
     var cue = cues[0];
-    console.log(cues[0]);
+    console.log(cue);
     console.log(cue.text);
     console.log(textTrack.kind);
 };
