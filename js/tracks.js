0b2bde702466c1f932c462747b2fa4137d0392a1
Juan Carlos Bujosa <85440338+jbherreros@users.noreply.github.com>
2022-03-26 22:17:25 +0100

    Update with tracks

--- js/tracks.js
+++ js/tracks.js
@@ -18,4 +18,31 @@ window.onload = function() {
     console.log(cue);
     console.log(cue.text);
     console.log(textTrack.kind);
+
+   /* cue.onenter = function(){
+        console.log("soy la cue entrando");
+       // console.log(JSON.parse(cue.text));
+    };
+
+    cue.onexit = function(){
+        console.log("soy la cue saliendo");
+    };*/
+
+    textTrack.oncuechange = function (){
+        var cue=textTrack.activeCues[0];
+        var obj = JSON.parse(cue.text);
+        // "this" is a textTrack
+        console.log("active " + obj.player.name);
+        document.getElementById('nombre').innerHTML= obj.player.name;
+        document.getElementById('player-pic').src="resources/"+obj.player.pic;
+        var cue = textTrack.activeCues[0]; // assuming there is only one active cue
+
+        var img = document.createElement("img");
+        img.src = "resources/"+obj.player.pic;
+        var src = document.getElementById("players-list");
+        src.appendChild(img);
+
+       }
+
 };
+
